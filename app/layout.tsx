4b765e80de282a363852eca843c214e24cc5dabc
Ram <rmmahour@gmail.com>
2026-01-20 15:40:35 +0530

    tags added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import "./globals.css"
+import Script from "next/script"
 import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google'
 import CookieConsent from "@/components/CookiesConsent"
 
@@ -19,6 +20,30 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" type="image/png" href="https://campaign.basiq360.com/loyalty-management-system/images/fevicon.png" />
+      
+        {/* Meta Pixel Code */}
+<Script
+  id="facebook-pixel"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '733484639407288');
+      fbq('track', 'PageView');
+    `,
+  }}
+/>
+<noscript><img height="1" width="1" style={{display:'none'}}
+src="https://www.facebook.com/tr?id=733484639407288&ev=PageView&noscript=1"
+/></noscript>
+ {/* End Meta Pixel Code  */}
       </head>
       <body className="antialiased">
         {children}
